de0e2d25fd0296066b0afdcd101fd65932eb45d2
mike911209 <mike911209@gmail.com>
2023-10-21 09:25:36 +0800

    modified

--- src/client/pages/Post.jsx
+++ src/client/pages/Post.jsx
@@ -37,8 +37,6 @@ const Post = () => {
 
   return (
     <div>
-      <h1>Welcome to the React App!</h1>
-      <p>This is a simple chat room web app.</p>
       <iframe src="https://showroom.one-stage.kkstream.io/embed?token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjE3N2RhODVjLTllZjgtNTVjYS05M2FkLTAyYTMyZjkwZjg2MyIsInR5cCI6IkpXVCJ9.eyJyZXNvdXJjZV9pZCI6ImMyZmZkM2U2LWMwYmYtNGMyZi1hOGU1LWU2MjgzY2IwMjk5NCIsInJlc291cmNlX3R5cGUiOiJSRVNPVVJDRV9UWVBFX0xJVkVfRVZFTlQifQ.aI9DdBikEi8H_06CRNNwc_248X5ECgclFjD_dRwdHkk8TSGJoMhG7RRTy1EmIV7qnPFszyE8K4Jxw2ygCMCiHCBiPnXdQ8G1ex2CWbMkYH6zGn60vf4jJI55KQah2wIFRTLFT2zPwW1HrMMHX_AqrIpsKgJKlM0jN9RFulUDhVQKbArAVxFctFgQOvhl2Ey5Exr_uoYwtmfoO-UsnyUo4OQ02ztgdYhd-OzuV_FgbxUB-A84b7DxTVVKvFj234Vq4PKJ59GcLNI2UNIwQ4OrqBcg7kQMYVAnJ55txHuirxGtOLXDC_Nw749EQUvwLyG9zgTyyXQShY_Hq2dzQmfqwMcl7bcWD_KjKEUAXP5VWQo9AoDNL3N7JROUpF_IbSdaiFAUajXEAbx8XGd8c67FLm17K-baOrM9Nep7_FvW3MoGkLVoUTNjDUCezwpZOu2ANe21iYvKNvpsQubkdpmUWjSGoNp34RICRoQ8m9s1yPy1T67amlNrkXWja6c-gJMSzxjLjDywD-2lL0PEvy5SYrXEemn17gBnAymup2J1anMmaOonWfX3eDH1PLLEoy-g1KjR4RF6DC44OZmf5JuIgRTRqrjcLzfSdMOcWOkcyhEfDdwMd0b_gHzMOedFWKeYfInI8Fs47GLtoZ1RNQ2k0ppGx9gyHUcERdLKjt4tpEw" 
       width="900" 
       height="500" 
