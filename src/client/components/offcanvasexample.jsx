36ceed163f89fc01e4746a08108589669d0e8ee8
daphne <daphneyang0127@gmail.com>
2023-10-22 12:06:19 +0800

    css

--- src/client/components/offcanvasexample.jsx
+++ src/client/components/offcanvasexample.jsx
@@ -14,13 +14,27 @@ const OffcanvasExample = (props) => {
 
   let button;
   if(props.authStatus==='authenticated'){
-    button = <Button onClick={async ()=>{props.signOut();}}>Sign out</Button>
+    button = <Button 
+    onClick={async ()=>{props.signOut();}}>Sign out</Button>
   }else{
-    button = <Button onClick={()=>{navigate('/login')}}>Sign in</Button>
+    button = <Button 
+    style={{backgroundColor: 'white', color: '#6495ed', borderColor: 'white' ,textAlign: 'left', padding: '0'}} 
+    onClick={()=>{navigate('/login')}}
+    onMouseEnter={(e) => {
+      e.target.style.backgroundColor = '#e6e6fa';
+      e.target.style.color = '#6495ed';
+    }}
+    onMouseLeave={(e) => {
+      e.target.style.backgroundColor = 'white';
+      e.target.style.color = '#6495ed';
+    }}>
+      
+      <span style={{ display: 'flex', alignItems: 'left' }}>Sign in</span>
+    </Button>
   }
 
   return (
-    <Navbar style={{ backgroundColor: '#00008b' }} expand={false} className="mb-3">
+    <Navbar style={{ backgroundColor: '#00008b' }} expand={false}>
       <Container fluid>
         <Navbar.Toggle aria-controls={`offcanvasNavbar`} />
         <Navbar.Brand href="#" style={{ color: 'white' }}>MChackathon07髮際線總是在跟我作隊</Navbar.Brand>
@@ -35,9 +49,28 @@ const OffcanvasExample = (props) => {
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Link className="nav-link" to='/'>Video</Link>
-              <Link className="nav-link" to='/post'>Streaming</Link>
+            <Nav className="justify-content-end flex-grow-1">
+              <Link className="nav-link" to='/' style={{ 
+                  color: '#6495ed',
+                  textDecoration: 'none', 
+                  transition: 'background-color 0.3s'
+
+                  // padding: 10,
+                  // margin: 8
+                }} 
+                onMouseEnter={(e) => e.target.style.backgroundColor = '#e6e6fa'} 
+                onMouseLeave={(e) => e.target.style.backgroundColor = 'white'}
+                >Video</Link>
+              <Link className="nav-link" to='/post'
+                style={{ 
+                  color: '#6495ed', 
+                  textDecoration: 'none', 
+                  transition: 'background-color 0.3s'
+                  // padding: 10,
+                  // margin: 8
+                }} onMouseEnter={(e) => e.target.style.backgroundColor = '#e6e6fa'} 
+                onMouseLeave={(e) => e.target.style.backgroundColor = 'white'}
+                >Streaming</Link>
             </Nav>
             {button}
           </Offcanvas.Body>
